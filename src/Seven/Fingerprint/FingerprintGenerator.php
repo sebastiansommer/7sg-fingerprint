4ef18c1fc45bd7aec31fc79bf0db4c699c711ba5
Sebastian Sommer <mail@sebastian-sommer.com>
2020-09-14 18:33:47 +0800

    Added option to specify cache for dd

--- src/Seven/Fingerprint/FingerprintGenerator.php
+++ src/Seven/Fingerprint/FingerprintGenerator.php
@@ -1,6 +1,7 @@
 <?php
 namespace Seven\Fingerprint;
 
+use DeviceDetector\Cache\Cache;
 use DeviceDetector\DeviceDetector;
 use DeviceDetector\Parser\Device\DeviceParserAbstract;
 
@@ -10,13 +11,19 @@ class FingerprintGenerator
      * Returns a SHA1 hash based on user agent an IP
      * @param string $userAgent
      * @param string $ip
+     * @param Cache|\Doctrine\Common\Cache\CacheProvider $cache
      * @return string
      */
-    public static function generate(string $userAgent, string $ip): string
+    public static function generate(string $userAgent, string $ip, $cache = null): string
     {
         DeviceParserAbstract::setVersionTruncation(DeviceParserAbstract::VERSION_TRUNCATION_NONE);
 
         $dd = new DeviceDetector($userAgent);
+
+        if ($cache !== null) {
+            $dd->setCache($cache);
+        }
+
         $dd->parse();
 
         $fingerprint = $dd->getOs('name') .
